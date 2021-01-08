652f92e726aacada9312f1911e262a4c5577113b
William Mead <william-mead@outlook.com>
2021-01-08 15:07:09 +0000

    Updated Lesson Creation design

--- tutor/lessons/views.py
+++ tutor/lessons/views.py
@@ -1,33 +1,44 @@
 # Create your views here.
 from django.views import generic
-from django.views.generic.edit import FormView
-from django.views.generic.edit import CreateView
-from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.views.generic.edit import FormView
+# from django.views.generic.edit import CreateView
+# from django.urls import reverse
 
-from .models import Lesson
+from .models import Lesson, Student
 
 
 class IndexView(generic.TemplateView):
     template_name = 'lessons/index.html'
 
 
-class LessonCreateView(CreateView):
-    model = Lesson
+class LessonCreateView(LoginRequiredMixin, generic.TemplateView):
     template_name = "lessons/lesson_create.html"
-    fields = ['student', 'date', 'duration_in_hours', 'topic', 'report', 'profile']
 
-    def get_success_url(self):
-        return reverse('lessons:view_lesson', kwargs={'pk': self.object.id})
+    def post(self, request, *args, **kwargs):
+        data = request.POST.dict()
 
-class LessonListView(generic.ListView):
+        lesson = Lesson()
+        lesson.student = Student.objects.get(pk=data["student"])
+        lesson.date = data["date"]
+        lesson.duration_in_hours = data["duration"]
+        lesson.topic = data["topic"]
+        lesson.report = data["report"]
+        lesson.profile = request.user.profile
+        lesson.save()
+
+        return self.get(self, request, *args, **kwargs)
+
+
+class LessonListView(LoginRequiredMixin, generic.ListView):
     model = Lesson
     context_object_name = 'lessons'
     queryset = Lesson.objects.all()
 
 
-class LessonDetailView(generic.DetailView):
+class LessonDetailView(LoginRequiredMixin, generic.DetailView):
     model = Lesson
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     return context
